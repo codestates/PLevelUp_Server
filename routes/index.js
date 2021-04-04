b1c7bf0a7c22b40f8fe50c5fceaa2b520f68ee1d
Napsztar <napsztar@gmail.com>
2021-04-05 03:08:21 +0900

    Implement master jwtMiddleware

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import master from './master';
+import masterJwtMiddleware from '../lib/masterJwtMiddleware';
 
 const router = express.Router();
 
+router.use(masterJwtMiddleware);
 router.use('/master', master);
 
 export default router;
