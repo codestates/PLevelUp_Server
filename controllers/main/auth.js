32023974acc5cf110285c268d60c61df4eb4905a
Outwater <kocon135@gmail.com>
2021-04-08 23:04:53 +0900

    Stop original-login

--- controllers/main/auth.js
+++ controllers/main/auth.js
@@ -2,6 +2,7 @@ import tokenFunction from '../tokenFunction';
 const { generateAccessToken, sendAccessToken } = tokenFunction;
 import models from '../../models';
 const { User } = models;
+import bcrypt from 'bcrypt';
 
 export default {
   //회원가입
@@ -13,45 +14,52 @@ export default {
       if (exists) {
         return res.status(409).send();
       }
-      const userData = await User.create({
+      const hashPassword = await bcrypt.hash(req.body.password, 10);
+      await User.create({
         email: req.body.email,
-        password: req.body.password,
+        password: hashPassword,
         username: req.body.username,
       });
-      const accessToken = generateAccessToken(userData.dataValues);
-      sendAccessToken(res, accessToken);
+      const user = await User.findByEmail(req.body.email);
+      const data = user.serialize();
+      const token = user.generateToken();
+      res.cookie('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 1,
+        httpOnly: true,
+      });
+      res.status(200).send(data);
     } catch (err) {
-      res.status(500).json({ message: err + '!' });
+      res.status(500).json({ message: err.toString() });
     }
   },
   //로그인
   login: async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(401).send();
+      return res.sendStatus(401);
     }
     try {
       await User.findOne({
         where: { email, password },
-      }).then(userData => {
-        if (!userData) {
-          return res.status(401).send();
-        }
-
-        delete userData.dataValues.password;
-        const accessToken = generateAccessToken(userData.dataValues); //email
-        sendAccessToken(res, accessToken);
       });
+      const user = await User.findByEmail(req.body.email);
+      const data = user.serialize();
+      const token = user.generateToken();
+      res.cookie('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 1,
+        httpOnly: true,
+      });
+      res.status(200).send(data);
     } catch (err) {
       res.status(500).json({ message: err + '!' });
     }
   },
   //로그인 상태확인
   check: async (req, res) => {
-    console.log('check here');
     const { user } = res;
     try {
       if (!user) {
+        console.log('user not exist');
         // user가 없다면 jwt검증에서 유저가 없는 것 = 로그인 중 아님
         return res.status(401).send();
       }
