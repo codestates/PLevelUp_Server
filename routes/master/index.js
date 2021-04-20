9d79b8f791c2576445180a59ac8670889409159b
Napsztar <napsztar@gmail.com>
2021-04-21 07:54:50 +0900

    Connect S3

--- routes/master/index.js
+++ routes/master/index.js
@@ -3,9 +3,11 @@ import club from './club';
 import auth from './auth';
 import masterCheckLoggedIn from '../../lib/masterCheckLoggedIn';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
 import fs from 'fs';
 import path from 'path';
 import { SERVER_HOST } from '../../app';
+import * as AWS from 'aws-sdk';
 
 const router = express.Router();
 
@@ -20,20 +22,43 @@ try {
   console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
   fs.mkdirSync('uploads');
 }
-const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
-router.post('/img', upload.single('img'), (req, res) => {
-  res.json({ url: `${SERVER_HOST}/img/${req.file.filename}` });
-});
+
+if (process.env.NODE_ENV === 'production') {
+  AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2',
+  });
+
+  const upload = multer({
+    storage: multerS3({
+      s3: new AWS.S3(),
+      bucket: 'p-levelup-client',
+      key(req, file, cb) {
+        cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
+      },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+  });
+  router.post('/img', upload.single('img'), (req, res) => {
+    res.json({ url: req.file.location });
+  });
+} else {
+  const upload = multer({
+    storage: multer.diskStorage({
+      destination(req, file, cb) {
+        cb(null, 'uploads/');
+      },
+      filename(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+  });
+  router.post('/img', upload.single('img'), (req, res) => {
+    res.json({ url: `${SERVER_HOST}/img/${req.file.filename}` });
+  });
+}
 
 export default router;
